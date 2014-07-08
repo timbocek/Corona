16584e1f743cba45f12cae96318cc8d40672f6f6
tbocek <tim.bocek@gmail.com>
2014-07-08 12:59:43 -0700

    Make the clock face activity display the sunrise and sunset times for Seattle, WA (currently used as the stub).

--- wear/src/main/java/com/tbocek/sunclock/ClockFaceActivity.java
+++ wear/src/main/java/com/tbocek/sunclock/ClockFaceActivity.java
@@ -3,22 +3,48 @@ package com.tbocek.sunclock;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.text.format.Time;
 import android.widget.TextView;
+
 
 public class ClockFaceActivity extends Activity {
 
-    private TextView mTextView;
+    private static final double SEATTLE_LAT = 47.609722;
+    private static final double SEATTLE_LONG = -122.333056;
+    private TextView mTimeTextView;
+    private TextView mSunriseTextView;
+    private TextView mSunsetTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clock_face);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTimeTextView = (TextView) findViewById(R.id.watch_face_time_text);
+                mSunriseTextView = (TextView) findViewById(R.id.watch_face_sunrise_text);
+                mSunsetTextView = (TextView) findViewById(R.id.watch_face_sunset_text);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        Time now = new Time();
+        now.setToNow();
+        SunriseTime sunriseTimeCalc = new SunriseTime(now, SEATTLE_LAT, SEATTLE_LONG,
+                SunriseTime.CIVIL_ZENITH);
+
+        Time sunriseTime = sunriseTimeCalc.getSunriseTime();
+        Time sunsetTime = sunriseTimeCalc.getSunsetTime();
+
+        sunriseTime.switchTimezone(now.timezone);
+        sunsetTime.switchTimezone(now.timezone);
+
+        mSunriseTextView.setText(sunriseTime.format("%H:%M:%S"));
+        mSunsetTextView.setText(sunriseTime.format("%H:%M:%S"));
     }
 }
