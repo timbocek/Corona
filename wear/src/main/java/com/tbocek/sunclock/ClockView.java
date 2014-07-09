7b37601e9f3e85e4f303d01749dda86789c05e6c
tbocek <tim.bocek@gmail.com>
2014-07-09 09:55:46 -0700

    Implimement clock view draw code.

--- wear/src/main/java/com/tbocek/sunclock/ClockView.java
+++ wear/src/main/java/com/tbocek/sunclock/ClockView.java
@@ -2,6 +2,8 @@ package com.tbocek.sunclock;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -16,6 +18,10 @@ public class ClockView extends View {
     private DateTime mSunriseTime;
     private DateTime mSunsetTime;
 
+    private Paint mHourHandPaint;
+    private Paint mMinuteHandPaint;
+    private Paint mSunriseSunsetHandPaint;
+
     public ClockView(Context context) {
         this(context, null, 0);
     }
@@ -26,6 +32,18 @@ public class ClockView extends View {
 
     public ClockView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+
+        mHourHandPaint = new Paint();
+        mHourHandPaint.setColor(Color.BLACK);
+        mHourHandPaint.setStrokeWidth(2.0f);
+
+        mMinuteHandPaint = new Paint();
+        mMinuteHandPaint.setColor(Color.BLACK);
+        mMinuteHandPaint.setStrokeWidth(1.0f);
+
+        mSunriseSunsetHandPaint = new Paint();
+        mSunriseSunsetHandPaint.setColor(Color.RED);
+        mSunriseSunsetHandPaint.setStrokeWidth(1.0f);
     }
 
     public DateTime getTime() {
@@ -34,6 +52,7 @@ public class ClockView extends View {
 
     public void setTime(DateTime mTime) {
         this.mTime = mTime;
+        this.invalidate();
     }
 
     public DateTime getSunriseTime() {
@@ -42,6 +61,7 @@ public class ClockView extends View {
 
     public void setSunriseTime(DateTime mSunriseTime) {
         this.mSunriseTime = mSunriseTime;
+        this.invalidate();
     }
 
     public DateTime getSunsetTime() {
@@ -50,18 +70,34 @@ public class ClockView extends View {
 
     public void setSunsetTime(DateTime mSunsetTime) {
         this.mSunsetTime = mSunsetTime;
+        this.invalidate();
     }
 
     public void onDraw(Canvas c) {
-        double hourHandDegrees = ((double)mTime.getHourOfDay()) / 24.0;
-        double minuteHandDegrees = ((double)mTime.getMinuteOfHour()) / 24.0;
+        c.drawColor(Color.WHITE);
+
+        int centerX = c.getWidth() / 2;
+        int centerY = c.getHeight() / 2;
+        int r = Math.min(c.getWidth(), c.getHeight());
+
+        drawHand(fractionOfDay(mTime), centerX, centerY, r, mHourHandPaint, c);
+        drawHand(fractionOfHour(mTime), centerX, centerY, r, mMinuteHandPaint, c);
+        drawHand(fractionOfDay(mSunriseTime), centerX, centerY, r, mSunriseSunsetHandPaint, c);
+        drawHand(fractionOfDay(mSunsetTime), centerX, centerY, r, mSunriseSunsetHandPaint, c);
     }
 
     private double fractionOfDay(DateTime t) {
-        return ((double)t.getHourOfDay() + fractionOfHour) / 24.0;
+        return ((double)t.getHourOfDay() + fractionOfHour(t)) / 24.0;
     }
 
     private double fractionOfHour(DateTime t) {
         return ((double) t.getMinuteOfHour()) / 60.0;
+    }
+
+    private void drawHand(double fraction, int centerX, int centerY, int r, Paint paint, Canvas c) {
+        int destX = centerX + r * (int) Math.cos(Math.PI / 2 - fraction * 2 * Math.PI);
+        int destY = centerY + r * (int) Math.sin(Math.PI / 2 - fraction * 2 * Math.PI);
+
+        c.drawLine(centerX, centerY, destX, destY, paint);
     }
 }
