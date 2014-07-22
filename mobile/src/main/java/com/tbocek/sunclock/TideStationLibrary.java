97c5c86950df3dd77849bbfcc80a65808ca724d7
tbocek <tim.bocek@gmail.com>
2014-07-22 12:49:15 -0700

    Implementation work on tide station library class.

--- mobile/src/main/java/com/tbocek/sunclock/TideStationLibrary.java
+++ mobile/src/main/java/com/tbocek/sunclock/TideStationLibrary.java
@@ -7,16 +7,19 @@ import android.util.Log;
 import com.tideengine.BackEndTideComputer;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
  * Created by tbocek on 7/21/14.
  */
 public class TideStationLibrary {
+
     public static class StationStub {
         private Location mLocation;
         private String mName;
-        private float mDistance;
+        private float mDistance = Float.NaN;
 
         public StationStub(Location location, String name) {
             mLocation = location;
@@ -54,6 +57,10 @@ public class TideStationLibrary {
         void stationsLoaded(TideStationLibrary stations, boolean loadSuccess);
     }
 
+    interface TideStationsFilterCallback {
+        void stationsLoaded(List<StationStub> foundStations);
+    }
+
     private static TideStationLibrary sInstance = null;
 
     public static TideStationLibrary instance() {
@@ -64,8 +71,15 @@ public class TideStationLibrary {
     }
 
     private boolean mStationsLoaded = false;
-    private List<TideStationsLoadedCallback> mStationsLoadedRunnables =
+
+    private List<TideStationsLoadedCallback> mStationsLoadedCallbacks =
             new ArrayList<TideStationsLoadedCallback>();
+
+    private List<TideStationsLoadedCallback> mLocationsComputedCallbacks =
+            new ArrayList<TideStationsLoadedCallback>();
+
+    private Location lastLocationUsed = null;
+
     private List<StationStub> mStations = new ArrayList<StationStub>();
 
     private TideStationLibrary() { }
@@ -74,17 +88,37 @@ public class TideStationLibrary {
         if (mStationsLoaded) {
             onLoaded.stationsLoaded(this, true);
         } else {
-            mStationsLoadedRunnables.add(onLoaded);
+            mStationsLoadedCallbacks.add(onLoaded);
             ensureLoading();
         }
     }
 
-    public synchronized void requestComputeDistances(Location location, Runnable onSuccess) {
+    public synchronized void requestComputeDistances(
+            Location location, TideStationsLoadedCallback onSuccess) {
+        if (lastLocationUsed != null && lastLocationUsed.distanceTo(location) < 30) {
+            onSuccess.stationsLoaded(this, true);
+            return;
+        }
 
+        mLocationsComputedCallbacks.add(onSuccess);
+        ensureComputingDistances();
     }
 
-    public synchronized void requestFilter() {
+    public synchronized void requestFilter(
+            float distance, String nameFragment, TideStationsFilterCallback cb) {
+        if (mStationFilterTask != null) {
+            mStationFilterTask.cancel(true);
+        }
+        mStationFilterTask = new StationFilterTask();
+        StationFilterParams params = new StationFilterParams();
+        params.distance = distance;
+        params.nameFragment = nameFragment;
+        params.filterCallback = cb;
+        mStationFilterTask.execute(params);
+    }
 
+    public List<StationStub> getAllStations() {
+        return mStations;
     }
 
     private synchronized void ensureLoading() {
@@ -93,14 +127,54 @@ public class TideStationLibrary {
         }
     }
 
+
+    private void ensureComputingDistances() {
+        if (mComputeDistancesTask.getStatus() != AsyncTask.Status.RUNNING) {
+            mComputeDistancesTask.execute();
+        }
+    }
+
     private class StationFilterParams {
         public String nameFragment;
         public float distance;
+        public TideStationsFilterCallback filterCallback;
     }
 
-    private class StationFilterTask extends AsyncTask<>
+    private class StationFilterResult {
+        public List<StationStub> foundStations = new ArrayList<StationStub>();
+        public TideStationsFilterCallback filterCallback;
+    }
 
-    private AsyncTask<Void, Void, Boolean> mLoadStationsTask = new AsyncTask<Void, Void, Boolean>() {
+    private class StationFilterTask
+            extends AsyncTask<StationFilterParams, Void, StationFilterResult> {
+
+        @Override
+        protected StationFilterResult doInBackground(StationFilterParams... params) {
+            StationFilterParams filterParams = params[0];
+            StationFilterResult result = new StationFilterResult();
+
+            String nameFragmentLower = filterParams.nameFragment.toLowerCase();
+            for (StationStub station: mStations) {
+                if (station.getDistance() > filterParams.distance) {
+                    break;
+                }
+                if (station.getName().toLowerCase().contains(nameFragmentLower)) {
+                    result.foundStations.add(station);
+                }
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(StationFilterResult result) {
+            result.filterCallback.stationsLoaded(result.foundStations);
+        }
+    }
+    private StationFilterTask mStationFilterTask;
+
+    private AsyncTask<Void, Void, Boolean> mLoadStationsTask =
+            new AsyncTask<Void, Void, Boolean>() {
         @Override
         protected Boolean doInBackground(Void... params) {
             try {
@@ -115,9 +189,42 @@ public class TideStationLibrary {
         @Override
         protected void onPostExecute(Boolean succeeded) {
             synchronized (this) {
-                for (TideStationsLoadedCallback r : mStationsLoadedRunnables) {
+                for (TideStationsLoadedCallback r : mStationsLoadedCallbacks) {
                     r.stationsLoaded(TideStationLibrary.this, succeeded);
                 }
+                mStationsLoadedCallbacks.clear();
+            }
+        }
+    };
+
+    private AsyncTask<Location, Void, Void> mComputeDistancesTask =
+            new AsyncTask<Location, Void, Void>() {
+
+        @Override
+        protected Void doInBackground(Location... params) {
+            Location location = params[0];
+
+            for (StationStub station: mStations) {
+                station.setDistance(station.getLocation().distanceTo(location));
+            }
+
+            Collections.sort(mStations, new Comparator<StationStub>() {
+
+                @Override
+                public int compare(StationStub lhs, StationStub rhs) {
+                    return Float.compare(lhs.getDistance(), rhs.getDistance());
+                }
+            });
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            synchronized (this) {
+                for (TideStationsLoadedCallback r : mLocationsComputedCallbacks) {
+                    r.stationsLoaded(TideStationLibrary.this, true);
+                }
+                mLocationsComputedCallbacks.clear();
             }
         }
     };
--- mobile/src/main/java/com/tbocek/sunclock/TideStationSelectorView.java
+++ mobile/src/main/java/com/tbocek/sunclock/TideStationSelectorView.java
@@ -1,13 +1,22 @@
 package com.tbocek.sunclock;
 
 import android.content.Context;
+import android.location.Location;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
+import java.util.List;
 
-public class TideStationSelectorView extends LinearLayout {
+
+public class TideStationSelectorView extends LinearLayout implements TextWatcher {
+
+    private EditText mDistance;
+    private EditText mFilter;
 
     public TideStationSelectorView(Context context) {
         super(context);
@@ -31,17 +40,69 @@ public class TideStationSelectorView extends LinearLayout {
 
         TideStationLibrary.instance().requestTideStations(
                 new TideStationLibrary.TideStationsLoadedCallback() {
+
                     @Override
                     public void stationsLoaded(TideStationLibrary stations, boolean loadSuccess) {
-                        findViewById(R.id.lookup_tide_station_station_list_loading).setVisibility(
-                                View.GONE
-                        );
-                        findViewById(R.id.lookup_tide_station_station_list).setVisibility(
-                                View.VISIBLE
-                        );
-
-
+                        setLoading(false);
                     }
         });
+
+        mDistance = (EditText) findViewById(R.id.lookup_tide_station_distance);
+        mFilter = (EditText) findViewById(R.id.lookup_tide_station_filter);
+
+        mDistance.addTextChangedListener(this);
+        mFilter.addTextChangedListener(this);
+    }
+
+    public void setLocation(final Location location) {
+        setLoading(true);
+        TideStationLibrary.instance().requestTideStations(
+                new TideStationLibrary.TideStationsLoadedCallback() {
+
+                    @Override
+                    public void stationsLoaded(TideStationLibrary stations, boolean loadSuccess) {
+                        stations.requestComputeDistances(location,
+                                new TideStationLibrary.TideStationsLoadedCallback() {
+                                    @Override
+                                    public void stationsLoaded(
+                                            TideStationLibrary stations, boolean loadSuccess) {
+                                        setLoading(false);
+                                        setStations(stations.getAllStations());
+                                    }
+                                });
+                    }
+                });
+    }
+
+    private void setStations(List<TideStationLibrary.StationStub> stations) {
+
+    }
+
+    private void setLoading(boolean loading) {
+        findViewById(R.id.lookup_tide_station_station_list_loading).setVisibility(
+                loading ? View.VISIBLE : View.GONE
+        );
+        findViewById(R.id.lookup_tide_station_station_list).setVisibility(
+                loading ? View.GONE : View.VISIBLE
+        );
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        TideStationLibrary.instance().requestFilter(
+                Float.parseFloat(mDistance.getText().toString()),
+                mFilter.getText().toString(),
+                new TideStationLibrary.TideStationsFilterCallback() {
+                    @Override
+                    public void stationsLoaded(List<TideStationLibrary.StationStub> foundStations) {
+                        setStations(foundStations);
+                    }
+                });
     }
 }
